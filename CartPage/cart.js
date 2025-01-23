81cf260c09cfdda5fe173752558959da4592ec84
Srikanth G <srikanthsrinivaspur10@gmail.com>
2025-01-23 02:10:36 -0800

    added css to cart page at initial state

--- CartPage/cart.js
+++ CartPage/cart.js
@@ -1,18 +1,18 @@
-
+// Function to update cart count (to be reused in both home.js and cart.js)
 function updateCartCount() {
     const cart = JSON.parse(localStorage.getItem('cart')) || {};
     const cartQuantity = Object.values(cart).reduce((total, item) => total + item.quantity, 0);
     const cartButton = document.getElementById('point3');
 
     if (cartQuantity > 0) {
-        
+        // If there are items in the cart, display the quantity
         cartButton.innerHTML = `
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
             </svg>
             <span class="cart-quantity"> Cart(${cartQuantity})</span>`;
     } else {
-     
+        // If there are no items in the cart, only show the icon
         cartButton.innerHTML = `
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5"/>
@@ -30,9 +30,11 @@ document.addEventListener('DOMContentLoaded', function() {
     
     if (Object.keys(cart).length === 0) {
         cartContainer.innerHTML = `<div class="godiv"> 
-        <h1 class="cartH">Your Cart is Empty</h1>
-         <button class="gobut" id="goBackButton">Continue Shopping</button>
-        <div>`;/
+        <p class="cartH">Your Cart is Empty</p>
+         <button class="gobut" id="goBackButton"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+</svg> Continue Shopping</button>
+        <div>`;
         document.getElementById('goBackButton').addEventListener('click', function() {
             window.location.href = '../HomePage/home.html';
         });
